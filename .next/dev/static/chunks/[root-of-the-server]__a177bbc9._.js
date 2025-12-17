fc2567405dcd3c013cb8093509eed3c530e6933b
Leila Lu <leila.lu@student.uib.no>
2025-12-17 20:49:02 +0100

    pulverheksa

--- .next/dev/static/chunks/[root-of-the-server]__a177bbc9._.js
+++ .next/dev/static/chunks/[root-of-the-server]__a177bbc9._.js
@@ -552,8 +552,8 @@ function HomePage() {
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                 className: "container max-w-4xl m-auto px-4 mt-20",
                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$client$5d$__$28$ecmascript$29$__["default"], {
-                    src: "/desk.jpg",
-                    alt: "my desk",
+                    src: "/profile.jpg",
+                    alt: "selvportrett",
                     width: 1920 / 2,
                     height: 1280 / 2
                 }, void 0, false, {
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,8 +29,8 @@ function HomePage() {
 
       <div className="container max-w-4xl m-auto px-4 mt-20">
         <Image
-          src="/desk.jpg"
-          alt="my desk"
+          src="/profile.jpg"
+          alt="selvportrett"
           width={1920 / 2}
           height={1280 / 2}
         />
